8b3ee59e1db96dd2fef037c2bfa6e0a2ab896dd2
Paulo Cesar <pauloc062@gmail.com>
2015-05-30 17:42:37 -0300

    refacor server creation

--- src/server.js
+++ src/server.js
@@ -7,48 +7,52 @@ var fs = require('fs'),
     AuthCookie = require('hapi-auth-cookie'),
     server = null;
 
-// set public folder to hapi
-var addPublicPath = function (server, publicPath) {
+var configureAuthStrategy = function (server, data) {
+    server.auth.strategy('session', 'cookie', {
+        cookie: 'session',
+        password: data.cookiePassword || 'harboria#&733b',
+        redirectTo: data.redirectLogin || '/login',
+        isSecure: false,
+        ttl: 24* 60 * 60 * 1000
+    });
+};
+
+var loadRoutes = function (server, data) {
+    var routes = harbor.helpers.requireFiles(data.routesPath);
+
+    //routes are equal Hapi structure
+    _.each(routes, function (r) { server.route(r); });
+};
+
+var addPublicPath = function (server, data) {
+    if (!data.publicPath) { return; }
+
     server.route({
         method: 'GET',
         path: '/{path*}',
-        handler: {directory: { path: publicPath, listing: true }}
+        handler: { directory: {
+            path: data.publicPath, listing: true
+        }}
     });
-}
+};
 
 // create server method
 module.exports = function (data) {
-
-    //HAPI server creation step
-    var hapiCfg = {};
-    server = new Hapi.Server(hapiCfg);
-
-    //start load routes and configs
     var harbor = GLOBAL.harbor,
+        hapiCfg = {},
+        server = new Hapi.Server(hapiCfg),
         register = Q.denodeify(_.bind(server.register, server)),
-        start = Q.denodeify(_.bind(server.start, server)),
-        routes = harbor.helpers.requireFiles(data.routesPath);
+        start = Q.denodeify(_.bind(server.start, server));
+
+    // set server to global access
+    harbor.hapi = server;
 
     server.connection({ port: data.port || 5105 });
 
-    // register hapi plugins and create routes
     return register(AuthCookie).then(function () {
-        server.auth.strategy('session', 'cookie', {
-            cookie: 'session',
-            password: data.cookiePassword || 'harboria#&733b',
-            redirectTo: data.redirectLogin || '/login',
-            isSecure: false,
-            ttl: 24* 60 * 60 * 1000
-        });
-
-        //routes are equal Hapi structure
-        _.each(routes, function (r) { server.route(r); });
-
-        if (data.publicPath) {
-            addPublicPath(server, data.publicPath);
-        }
-
-        harbor.hapi = server;
+        configureAuthStrategy(server, data);
+        loadRoutes(server, data);
+        addPublicPath(server, data);
 
         return start();
     })
